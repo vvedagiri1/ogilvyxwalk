371fd6c7cb40ca0a3e3501b132aeb878fa49325f
vvedagiri1 <161562723+vvedagiri1@users.noreply.github.com>
2026-02-10 14:09:52 +0530

    Update quote.js

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -3,13 +3,13 @@ export default function decorate(block) {
   //block.textContent = ''; // Clear the block
 
   // 2. Fetch weather data (Example using a placeholder API)
-  const API_KEY = '3237161ae1634c9a8e051934260902';
+ /* const API_KEY = '3237161ae1634c9a8e051934260902';
   const loc = 'ny';
   const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${loc}`);
   const data = await response.json();
 
   // 3. Create HTML structure
-  /*const weatherHTML = `
+  const weatherHTML = `
     <div class="weather-card">
       <h3>${data.location.name}</h3>
       <img src="${data.current.condition.icon}" alt="${data.current.condition.text}" />
